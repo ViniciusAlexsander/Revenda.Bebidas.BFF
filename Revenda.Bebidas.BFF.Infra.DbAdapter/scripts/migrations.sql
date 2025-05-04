881107e026cb22dfddd8879b0b6f37b208bb2bfa
Vinicius Marinho <viniciusalexsander1@gmail.com>
2025-05-04 18:58:47 -0300

    chore: add scripts sql and readme

---
+++ Revenda.Bebidas.BFF.Infra.DbAdapter/scripts/migrations.sql
@@ -0,0 +1,67 @@
+﻿CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE revendas (
+    id UUID PRIMARY KEY,
+    cnpj VARCHAR(18) NOT NULL,
+    razao_social  VARCHAR(255) NOT NULL,
+    nome_fantasia  VARCHAR(255),
+    email VARCHAR(255),
+    telefone VARCHAR(20),
+    nome_contato VARCHAR(255),
+    endereco_entrega VARCHAR(255),
+    criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    atualizado_em TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE produtos (
+    id UUID PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    descricao TEXT,
+    preco NUMERIC(10, 2) NOT NULL,
+    unidade VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE pedidos (
+    id UUID PRIMARY KEY,
+    cliente_id UUID REFERENCES clientes(id) ON DELETE CASCADE,
+    revenda_id UUID REFERENCES revendas(id) ON DELETE CASCADE,
+    criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    status VARCHAR(50) DEFAULT 'pendente'
+);
+
+CREATE TABLE itens_pedido (
+    id UUID PRIMARY KEY,
+    pedido_id UUID NOT NULL REFERENCES pedidos(id) ON DELETE CASCADE,
+    produto_id UUID NOT NULL REFERENCES produtos(id),
+    quantidade INT NOT NULL CHECK (quantidade > 0)
+);
+
+CREATE TABLE clientes (
+    id UUID PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    email VARCHAR(255),
+    telefone VARCHAR(20),
+    criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE PedidosFabrica (
+    id UUID PRIMARY KEY,
+    revenda_id UUID NOT NULL,
+    criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    status VARCHAR(20) NOT NULL, --"Pendente", "Processando", "Enviado", "Erro"
+    tentativas_envio INT NOT NULL DEFAULT 0,
+    atualizado_em TIMESTAMP NOT NULL DEFAULT NOW(),
+    mensagem_erro VARCHAR NULL
+);
+
+CREATE TABLE pedidos_fabrica_para_pedidos_cliente (
+    id UUID PRIMARY KEY,
+    pedido_fabrica_id UUID NOT NULL REFERENCES pedidosfabrica(id),
+    pedido_cliente_id UUID NOT NULL REFERENCES pedidos(id)
+);
+
+INSERT INTO produtos (id, nome, descricao, preco, unidade)
+VALUES (uuid_generate_v4(), 'Cerveja Pilsen 600ml', 'Cerveja clara', 5.99, 'UN');
+
+INSERT INTO produtos (id, nome, descricao, preco, unidade)
+VALUES (uuid_generate_v4(), 'Guaraná', 'Refrigerante de guaraná', 4.50, 'UN');
---
+++ Revenda.Bebidas.WebApi/README.md
@@ -0,0 +1,40 @@
+﻿# Revenda.Bebidas.BFF
+
+Este projeto utiliza **PostgreSQL** como banco de dados principal. Por isso, é necessário configurar adequadamente a string de conexão e executar os scripts de estrutura inicial do banco antes de iniciar a aplicação.
+
+## 🔧 Configuração do Banco de Dados
+
+1. **Configure a Connection String**
+
+   No arquivo de configuração da aplicação (por exemplo, `appsettings.Development.json`), defina a string de conexão com suas credenciais do PostgreSQL:
+
+   ```json
+   "ConnectionStrings": {
+     "DefaultConnection": "Host=localhost;Port=5432;Database=revenda_bebidas;Username=seu_usuario;Password=sua_senha"
+   }
+   ```
+
+2. **Crie o banco e execute os scripts**
+
+   Antes de rodar o projeto, é necessário criar a estrutura do banco de dados.
+
+   Execute os scripts SQL disponíveis na pasta:
+
+   ```
+   Revenda.Bebidas.BFF.Infra.DbAdapter/Scripts/
+   ```
+
+   Os scripts criam as tabelas, extensões necessárias (`uuid-ossp`), e relações entre as entidades (revendas, pedidos, itens, etc.).
+
+   Você pode executá-los utilizando um cliente SQL, como:
+
+   * **psql** (terminal)
+   * **DBeaver**
+   * **TablePlus**
+   * **PgAdmin**
+
+## Considerações
+- Sobre a minha implementação:
+    - Decidi por usar a arquitetura hexagonal, que é algo uma arquitetura que trabalhei por um tempo.
+    - Optei por usar um background service para organizar a fila em casos de falha, tentei a implementação utilizando serviceBus porem me deparei com problemas de compatibilidade na hora de rodar localmente, por não ter muito sobrando, fiz a troca para usar o background service pois é algo que já implementei no passado.
+    - Não fiquei muito satisfeito com a implementação, ficou faltando algumas coisas para chegar em resultado que eu considere bom, o tempo não foi suficiente para fazer um melhor tratamento dos dados de entrada, não consegui ter tempo de implementar os testes, poderia adicionar uma forma de autenticação, foquei apenas em entregar o suficiente para que a aplicação fizesse o que foi pedido.
